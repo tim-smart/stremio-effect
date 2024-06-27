4888a7f80df0bf65870c1a7d0e9e1c9c40c36138
Tim <hello@timsmart.co>
2024-06-27 21:37:13 +1200

    add PORT config

--- src/main-bun.ts
+++ src/main-bun.ts
@@ -1,6 +1,6 @@
 import { BunHttpServer, BunRuntime } from "@effect/platform-bun"
 import * as Stremio from "./Stremio.js"
-import { Layer, Logger, LogLevel } from "effect"
+import { Config, Layer, Logger, LogLevel } from "effect"
 import { HttpClient } from "@effect/platform"
 import { TracingLive } from "./Tracing.js"
 import { AllSourcesDebrid } from "./Sources/All.js"
@@ -17,7 +17,11 @@ const HttpLive = Stremio.layerAddon.pipe(
       types: ["movie", "tv", "series"],
     }),
   ),
-  Layer.provide(BunHttpServer.layer({ port: 8000 })),
+  Layer.provide(
+    BunHttpServer.layerConfig({
+      port: Config.integer("PORT").pipe(Config.withDefault(8000)),
+    }),
+  ),
   Layer.provide(AllSourcesDebrid),
   Layer.provide(HttpClient.layer),
 )
