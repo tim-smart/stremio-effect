e0f441c16de0a6abf9be6c684032f5d1a77ac3d8
Tim <hello@timsmart.co>
2024-06-28 22:06:52 +1200

    honeycomb

--- src/Tracing.ts
+++ src/Tracing.ts
@@ -1,22 +1,33 @@
 import * as NodeSdk from "@effect/opentelemetry/NodeSdk"
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"
-import { Config, Effect, Layer, Option } from "effect"
+import { Config, Effect, Layer, Redacted } from "effect"
 
 export const TracingLive = Layer.unwrapEffect(
-  Effect.gen(function* (_) {
-    const endpoint = yield* Config.option(
-      Config.string("OTEL_EXPORTER_OTLP_ENDPOINT"),
+  Effect.gen(function* () {
+    const apiKey = yield* Config.option(Config.redacted("HONEYCOMB_API_KEY"))
+    const dataset = yield* Config.withDefault(
+      Config.string("HONEYCOMB_DATASET"),
+      "stremio-effect",
     )
-    if (Option.isNone(endpoint)) {
+    if (apiKey._tag === "None") {
       return Layer.empty
     }
+
+    const headers = {
+      "X-Honeycomb-Team": Redacted.value(apiKey.value),
+      "X-Honeycomb-Dataset": dataset,
+    }
+
     return NodeSdk.layer(() => ({
       resource: {
-        serviceName: "stremio-effect",
+        serviceName: dataset,
       },
       spanProcessor: new BatchSpanProcessor(
-        new OTLPTraceExporter({ url: `${endpoint.value}/v1/traces` }),
+        new OTLPTraceExporter({
+          url: "https://api.honeycomb.io/v1/traces",
+          headers,
+        }),
       ),
     }))
   }),
