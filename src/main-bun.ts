85095aa3deaf8842d872863916f1113d7f902755
Tim <hello@timsmart.co>
2024-10-11 13:43:27 +1300

    cleanup main

--- src/main-bun.ts
+++ src/main-bun.ts
@@ -1,36 +1,17 @@
 import { BunHttpServer, BunRuntime } from "@effect/platform-bun"
-import * as Stremio from "./Stremio.js"
 import { Config, Layer, Logger, LogLevel } from "effect"
 import { TracingLive } from "./Tracing.js"
-import { AllSourcesDebrid } from "./Sources/All.js"
-import { PersistenceLive } from "./Persistence.js"
 import { FetchHttpClient } from "@effect/platform"
+import { AddonLive } from "./Addon.js"
 
-const StremioLive = Stremio.layerAddon.pipe(
-  Layer.provide(
-    Stremio.StremioManifest.layer({
-      id: "co.timsmart.stremio.sources",
-      name: "Stremio Sources",
-      version: "0.0.1",
-      description: "Stream results from various sources",
-      catalogs: [],
-      resources: ["stream"],
-      types: ["movie", "tv", "series"],
-    }),
-  ),
+const MainLive = AddonLive.pipe(
   Layer.provide(
     BunHttpServer.layerConfig({
       port: Config.integer("PORT").pipe(Config.withDefault(8000)),
     }),
   ),
-  Layer.provide(AllSourcesDebrid),
   Layer.provide(FetchHttpClient.layer),
-  Layer.provide(PersistenceLive),
-)
-
-const MainLive = StremioLive.pipe(
   Layer.provide(TracingLive),
-  Layer.provide(Logger.pretty),
   Layer.provide(Logger.minimumLogLevel(LogLevel.All)),
 )
 
--- src/main.ts
+++ src/main.ts
@@ -3,38 +3,22 @@ import {
   NodeHttpServer,
   NodeRuntime,
 } from "@effect/platform-node"
-import * as Stremio from "./Stremio.js"
 import { Config, Layer, Logger, LogLevel } from "effect"
 import { TracingLive } from "./Tracing.js"
-import { AllSourcesDebrid } from "./Sources/All.js"
 import { createServer } from "node:http"
 import * as Net from "node:net"
+import { AddonLive } from "./Addon.js"
 
 // Fixes issues with timeouts
 Net.setDefaultAutoSelectFamily(false)
 
-const StremioLive = Stremio.layerAddon.pipe(
-  Layer.provide(
-    Stremio.StremioManifest.layer({
-      id: "co.timsmart.stremio.sources",
-      name: "Stremio Sources",
-      version: "0.0.1",
-      description: "Stream results from various sources",
-      catalogs: [],
-      resources: ["stream"],
-      types: ["movie", "tv", "series"],
-    }),
-  ),
-  Layer.provide(
+const MainLive = AddonLive.pipe(
+  Layer.provide([
     NodeHttpServer.layerConfig(createServer, {
       port: Config.integer("PORT").pipe(Config.withDefault(8000)),
     }),
-  ),
-  Layer.provide(AllSourcesDebrid),
-  Layer.provide(NodeHttpClient.layerUndici),
-)
-
-const MainLive = StremioLive.pipe(
+    NodeHttpClient.layerUndici,
+  ]),
   Layer.provide(TracingLive),
   Layer.provide(Logger.minimumLogLevel(LogLevel.All)),
 )
